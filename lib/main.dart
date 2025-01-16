102f346858390acc40a3c2c89f5060ab2bbf94f6
deepakmewada <mewadadeepak367@gmail.com>
2025-01-16 14:30:20 +0530

    I add Dark Mode into the App using colorscheme

--- lib/main.dart
+++ lib/main.dart
@@ -5,32 +5,56 @@ var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 181),
 );
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
+
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 5,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
       theme: ThemeData().copyWith(
         colorScheme: kColorScheme,
         appBarTheme: const AppBarTheme().copyWith(
             backgroundColor: kColorScheme.onPrimaryContainer,
             foregroundColor: kColorScheme.primaryContainer),
         cardTheme: const CardTheme().copyWith(
-            color: kColorScheme.secondaryContainer,
-            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 5)),
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 5,
+          ),
+        ),
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
             backgroundColor: kColorScheme.primaryContainer,
           ),
         ),
         textTheme: ThemeData().textTheme.copyWith(
-          titleLarge: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 18,
-            color: kColorScheme.onSecondaryContainer
-
-          )
-        ),
+            titleLarge: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+                color: kColorScheme.onSecondaryContainer)),
       ),
+      themeMode: ThemeMode.system,
       home: const Expences(),
     ),
   );
--- lib/widgets/expences-list/daily_expences_list.dart
+++ lib/widgets/expences-list/daily_expences_list.dart
@@ -3,16 +3,22 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker_app/modal/expence.dart';
 
 class DailyExpencesList extends StatelessWidget {
-  const DailyExpencesList({super.key, required this.expencesList,required this.onRemoveExpence});
+  const DailyExpencesList(
+      {super.key, required this.expencesList, required this.onRemoveExpence});
   final List<Expence> expencesList;
-  final void Function(Expence expence) onRemoveExpence; 
+  final void Function(Expence expence) onRemoveExpence;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expencesList.length,
       itemBuilder: (ctx, index) => Dismissible(
-        onDismissed: (direction) => onRemoveExpence(expencesList[index]),
+          onDismissed: (direction) => onRemoveExpence(expencesList[index]),
+          background: Container(
+            color: Theme.of(context).colorScheme.error.withAlpha(15).withOpacity(0.20),
+            //margin:EdgeInsets.symmetric(horizontal: Theme.of(context).cardTheme.margin!.horizontal,vertical: Theme.of(context).cardTheme.margin!.vertical),
+            margin: Theme.of(context).cardTheme.margin,
+          ),
           key: ValueKey(expencesList[index]),
           child: ExpenceItem(expencesList[index])),
     );
