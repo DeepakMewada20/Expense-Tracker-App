6ffcfa74b2293915602631ffaaa8ee6c89733645
deepakmewada <mewadadeepak367@gmail.com>
2025-01-22 21:12:56 +0530

    learn how to show differnet contect acording to differant platform like IOS,linux,android etc I show alert dilog acording to diffrent pletform

--- lib/widgets/expences-list/new_expense.dart
+++ lib/widgets/expences-list/new_expense.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:expense_tracker_app/widgets/expences-list/input_filed_for_new_expense.dart';
 import 'package:expense_tracker_app/widgets/expences-list/titalFiled.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker_app/modal/expence.dart';
 
@@ -42,12 +45,25 @@ class _NewExpenseState extends State<NewExpense> {
     });
   }
 
-  void _submmitExpenseData() {
-    final enteredamount = double.tryParse(_amountController.text);
-    bool amountIsInvalid = enteredamount == null || enteredamount <= 0;
-    if (_titalController.text.trim().isEmpty ||
-        amountIsInvalid ||
-        _selectedDate == null) {
+  void _showDialog() {
+    if (Platform.isIOS) {
+      showCupertinoDialog(
+        context: context,
+        builder: (ctx) => CupertinoAlertDialog(
+          title: const Text("Invelid Input"),
+          content: const Text(
+              "Please make sure a valid tital, amount, Date, and category was entered"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(ctx);
+              },
+              child: const Text("Okey"),
+            ),
+          ],
+        ),
+      );
+    } else {
       showDialog(
         context: context,
         builder: (ctx) => AlertDialog(
@@ -64,6 +80,16 @@ class _NewExpenseState extends State<NewExpense> {
           ],
         ),
       );
+    }
+  }
+
+  void _submmitExpenseData() {
+    final enteredamount = double.tryParse(_amountController.text);
+    bool amountIsInvalid = enteredamount == null || enteredamount <= 0;
+    if (_titalController.text.trim().isEmpty ||
+        amountIsInvalid ||
+        _selectedDate == null) {
+      _showDialog();
       return;
     }
     widget.onAddExpence(
@@ -90,74 +116,110 @@ class _NewExpenseState extends State<NewExpense> {
   @override
   Widget build(BuildContext context) {
     final keybordSpace = MediaQuery.of(context).viewInsets.bottom;
-    return SizedBox(
-      height: double.infinity,
-      child: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.fromLTRB(16, 16, 16, keybordSpace + 16),
-          child: Column(
-            children: [
-              Titalfiled(controller: _titalController),
-              Row(
+    return LayoutBuilder(
+      builder: (ctx, constrains) {
+        final width = constrains.maxWidth;
+        return SizedBox(
+          height: double.infinity,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(16, 16, 16, keybordSpace + 16),
+              child: Column(
                 children: [
-                  Inputfiled.amountFiled(
-                    controller: _amountController,
-                  ),
+                  if (width >= 600)
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: Titalfiled(
+                            controller: _titalController,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 25,
+                        ),
+                        Inputfiled.amountFiled(
+                          controller: _amountController,
+                        ),
+                      ],
+                    )
+                  else
+                    Titalfiled(controller: _titalController),
+                  if (width >= 600)
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Inputfiled.dropDownButten(
+                          selectdCategory: _selectdCategory,
+                          selectDropdownItam: _selectDropdownItam,
+                        ),
+                        const SizedBox(
+                          width: 25,
+                        ),
+                        Inputfiled.datePecker(
+                            selectedDate: _selectedDate,
+                            presentDatePeker: _presentDatePeker),
+                      ],
+                    )
+                  else
+                    Row(
+                      children: [
+                        Inputfiled.amountFiled(
+                          controller: _amountController,
+                        ),
+                        const SizedBox(
+                          width: 25,
+                        ),
+                        Inputfiled.datePecker(
+                            selectedDate: _selectedDate,
+                            presentDatePeker: _presentDatePeker),
+                      ],
+                    ),
                   const SizedBox(
-                    width: 16,
+                    height: 20,
                   ),
-                  Inputfiled.datePecker(
-                      selectedDate: _selectedDate,
-                      presentDatePeker: _presentDatePeker),
+                  if (width >= 600)
+                    Row(
+                      children: [
+                        const Spacer(),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text("Censal"),
+                        ),
+                        ElevatedButton(
+                          onPressed: _submmitExpenseData,
+                          child: const Text("Save Expense"),
+                        ),
+                      ],
+                    )
+                  else
+                    Row(
+                      children: [
+                        Inputfiled.dropDownButten(
+                          selectdCategory: _selectdCategory,
+                          selectDropdownItam: _selectDropdownItam,
+                        ),
+                        const Spacer(),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text("Censal"),
+                        ),
+                        ElevatedButton(
+                          onPressed: _submmitExpenseData,
+                          child: const Text("Save Expense"),
+                        ),
+                      ],
+                    )
                 ],
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                children: [
-                  // DropdownButton(
-                  //   value: _selectdCategory,
-                  //   items: Category.values
-                  //       .map(
-                  //         (category) => DropdownMenuItem(
-                  //           value: category,
-                  //           child: Text(
-                  //             category.name.toUpperCase(),
-                  //           ),
-                  //         ),
-                  //       )
-                  //       .toList(),
-                  //   onChanged: (value) {
-                  //     setState(() {
-                  //       if (value == null) {
-                  //         return;
-                  //       }
-                  //       setState(() {
-                  //         _selectdCategory = value;
-                  //       });
-                  //     });
-                  //   },
-                  // ),
-                  Inputfiled.dropDownButten(
-                      selectdCategory: _selectdCategory,
-                      selectDropdownItam: _selectDropdownItam),
-                  const Spacer(),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: const Text("Censal"),
-                  ),
-                  ElevatedButton(
-                      onPressed: _submmitExpenseData,
-                      child: const Text("Save Expense"))
-                ],
-              )
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
